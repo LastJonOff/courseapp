6e836f65c2ef491b1841dd9cb3f73b48241f1851
LastJon <zaicev-e@bk.ru>
2020-02-24 14:46:46 +0300

    my first commit

--- config/db.js
+++ config/db.js
@@ -2,3 +2,4 @@ module.exports = {
   db: 'mongodb+srv://LastJon:1453Tula@cluster0-2gcrk.mongodb.net/test?retryWrites=true&w=majority',
   secret:"some_some"
 }
+//mongodb+srv://LastJon:1453Tula@cluster0-2gcrk.mongodb.net/test?retryWrites=true&w=majority
